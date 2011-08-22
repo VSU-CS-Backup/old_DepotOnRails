1b3b211090c2c800674695fb500305645928409d
Zhiguang Xu <xuzhigaung@gmail.com>
2011-08-22 15:48:20 -0400

    Iteration A1: done but "rake test"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '77a550e61520f3777b76d87059a6ca326d2a114064ec039c3fbd87ed017e61d56b1696ab09d7a8b956136c11e9d2a929b78bb23f425ef91637647262670cd047'
