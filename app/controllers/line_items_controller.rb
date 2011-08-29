ddf7df8a65f229bf75f2edf0d48eaafc91a2a44d
Zhiguang Xu <xuzhigaung@gmail.com>
2011-08-29 13:32:29 -0400

    Iteration F1 done.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -43,14 +43,14 @@ class LineItemsController < ApplicationController
     @cart = current_cart
     product = Product.find(params[:product_id])
     
-    session[:counter]=0
+    # session[:counter]=0
 
     @line_item = @cart.add_product(product.id)
 
     
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to(@line_item.cart) }
+        format.html { redirect_to(store_url) }
         format.xml { render :xml => @line_item,:status => :created, :location => @line_item }
       else
         format.html { render :action => "new" }
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,11 +1,13 @@
 class StoreController < ApplicationController
   def index
     @products = Product.all
-    if session[:counter].nil?
-      session[:counter] = 0
-    else
-      session[:counter] = session[:counter]+1
-    end
+    @cart = current_cart
+    
+    # if session[:counter].nil?
+      # session[:counter] = 0
+    # else
+      # session[:counter] = session[:counter]+1
+    # end
 
   end
 
