3d5e79ac3f2d50b6f79aaf7492c5531bb186733b
Zhiguang Xu <xuzhiguang@gmail.com>
2011-08-31 12:28:19 -0400

    Revert "Task G, Playtime -- payment types in a database table. Done."
    This reverts commit 9c15bc05899105ec6639555dcbf12767a4e373fa.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,8 @@
 class Order < ActiveRecord::Base
-  # PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order" ]
+  PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order" ]
   # ...
   validates :name, :address, :email, :pay_type, :presence => true
-  validates :pay_type, :inclusion => PaymentType.names
+  validates :pay_type, :inclusion => PAYMENT_TYPES
 
   has_many :line_items, :dependent => :destroy
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110831023444) do
+ActiveRecord::Schema.define(:version => 20110829195807) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at"
@@ -31,12 +31,6 @@ ActiveRecord::Schema.define(:version => 20110831023444) do
     t.text     "address"
     t.string   "email"
     t.string   "pay_type"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "payment_types", :force => true do |t|
-    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
