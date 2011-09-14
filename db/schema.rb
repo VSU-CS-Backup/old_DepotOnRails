207e30614c4b8836962f8ce1445d150f98a70f86
Zhiguang Xu <xuzhiguang@gmail.com>
2011-09-14 10:41:01 -0400

    Getting Ready...Playtime_F

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110826200632) do
+ActiveRecord::Schema.define(:version => 20110908171217) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(:version => 20110826200632) do
     t.decimal  "price",       :precision => 8, :scale => 2
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.decimal  "popularity",                                :default => 0.0
   end
 
 end
